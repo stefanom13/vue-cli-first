b274c00401e6c991eb6edf5dfc15a6f0f14c8946
stefanom13 <stefanomancuso13@gmail.com>
2022-03-08 16:49:17 +0100

    inserimento title

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyTitle />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import MyTitle from './components/MyTitle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyTitle,
   }
 }
 </script>
---
+++ src/components/MyTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h2>La mia prima app con Vue CLI!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"MyTitle",
+}
+</script>
+
+<style>
+
+</style>
